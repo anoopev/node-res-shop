bdd6ff5a1d4f7bd78bff3d0e4c081217e9c9b772
Anoop Varghese <anoopedicula@gmail.com>
2019-09-21 21:16:01 +0530

    Delete metadata & error handling, error handling for GET /:orderId

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -80,6 +80,11 @@ router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
     .exec()
     .then(order => {
+        if (!order) {
+            return res.status(404).json({
+                message: "Order not found!"
+            });
+        }
         res.status(200).json({
             message: "Order Found",
             order: order,
@@ -98,9 +103,22 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.delete('/:orderId', (req, res, next) => {
+    Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
     res.status(200).json({
-        message: 'Order deleted!',
-        orderId: req.params.orderId
+        message: 'Order deleted successfully!',
+        request: {
+            type: 'POST',
+            URL: 'http:localhost:3000/orders',
+            body: { productId: 'ID', quantity: 'Number'}
+        }
+    })        
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
