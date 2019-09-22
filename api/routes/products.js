0640761c749d094e9a649746c48a58cd5d6b63ca
Anoop Varghese <anoopedicula@gmail.com>
2019-09-22 15:02:21 +0530

    Added file upload to uploads folder with filter

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,48 +2,78 @@ const express = require('express');
 
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // accept a file
+
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    }
+    // reject a file
+    else {
+        cb(null, false);
+    }
+};
+const upload = multer({
+    storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
-    .select('name price _id')
-    .exec()
-    .then(docs => {
-        console.log("All Products from DB: ", docs);
-        const response = {
-            count: docs.length,
-            products: docs.map(doc => {
-                return {
-                    name: doc.name,
-                    price: doc.price,
-                    _id: doc._id,
-                    request: {
-                        type: 'GET',
-                        URL: 'http://localhost:3000/products/' + doc._id
+        .select('name price _id')
+        .exec()
+        .then(docs => {
+            console.log("All Products from DB: ", docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            URL: 'http://localhost:3000/products/' + doc._id
+                        }
                     }
-                }
-            })
-        }
-        if (docs.length >= 0) {
-            res.status(200).json(response);
-        }
-        else {
-            res.status(404).json({
-                message: "No entries found!"
+                })
+            }
+            if (docs.length >= 0) {
+                res.status(200).json(response);
+            }
+            else {
+                res.status(404).json({
+                    message: "No entries found!"
+                });
+            }
+
+        })
+        .catch(err => {
+            console.log("Error when fetching all products from DB: ", err);
+            res.status(500).json({
+                error: err
             });
-        }
-       
-    })
-    .catch(err => {
-        console.log("Error when fetching all products from DB: ", err);
-        res.status(500).json({
-            error: err
         });
-    });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+    console.log("Product Image: ", req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -64,40 +94,40 @@ router.post('/', (req, res, next) => {
             }
         });
     })
-    .catch(err => {
-        console.log("Product POST Error: ", err);
-        res.status(500).json({
-            error: err
+        .catch(err => {
+            console.log("Product POST Error: ", err);
+            res.status(500).json({
+                error: err
+            });
         });
-    });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-    .select('name price _id')
-    .exec()
-    .then( doc => {
-        console.log("Product Find result from database: ", doc);
-        if (doc) {
-            res.status(200).json({
-                product: doc,
-                request: {
-                    type: 'GET',
-                    description: 'GET_ALL_PRODUCTS',
-                    URL: 'http:/localhost:3000/products'
-                }
-            });
-        }
-        else {
-            res.status(404).json({ message: "Product doesn't exist with this id!"});
-        }
-        
-    })
-    .catch(err => {
-        console.log("Error in Product Find: ", err);
-        res.status(500).json({ error: err });
-    });
+        .select('name price _id')
+        .exec()
+        .then(doc => {
+            console.log("Product Find result from database: ", doc);
+            if (doc) {
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        description: 'GET_ALL_PRODUCTS',
+                        URL: 'http:/localhost:3000/products'
+                    }
+                });
+            }
+            else {
+                res.status(404).json({ message: "Product doesn't exist with this id!" });
+            }
+
+        })
+        .catch(err => {
+            console.log("Error in Product Find: ", err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.patch('/:productId', (req, res, next) => {
@@ -107,45 +137,45 @@ router.patch('/:productId', (req, res, next) => {
         updateOps[ops.propName] = ops.value;
     }
     Product.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result => {
-        console.log("Patch request to DB result: ", result);
-        res.status(200).json({
-            message: 'Product updated successfully!',
-            request: {
-                type: 'GET',
-                URL: 'http://localhost:3000/products/' + id
-            }
+        .exec()
+        .then(result => {
+            console.log("Patch request to DB result: ", result);
+            res.status(200).json({
+                message: 'Product updated successfully!',
+                request: {
+                    type: 'GET',
+                    URL: 'http://localhost:3000/products/' + id
+                }
+            });
+        })
+        .catch(err => {
+            console.log("Error when PATCH to DB: ", err);
+            res.status(500).json({
+                error: err
+            });
         });
-    })
-    .catch(err => {
-        console.log("Error when PATCH to DB: ", err);
-        res.status(500).json({
-            error: err
-        });
-    });
 });
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.remove({_id: id})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'Product deleted successfully!',
-            request: {
-                type: 'POST',
-                URL: 'http://localhost:3000/products',
-                body: { name: 'String', price: 'Number' }
-            }
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Product deleted successfully!',
+                request: {
+                    type: 'POST',
+                    URL: 'http://localhost:3000/products',
+                    body: { name: 'String', price: 'Number' }
+                }
+            });
+        })
+        .catch(err => {
+            console.log("Error when deleting from DB: ".err);
+            res.status(500).json({
+                error: err
+            });
         });
-    })
-    .catch(err => {
-        console.log("Error when deleting from DB: ". err);
-        res.status(500).json({
-            error: err
-        });
-    });
 });
 
 module.exports = router;
