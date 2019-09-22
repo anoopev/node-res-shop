867dfe3897f107c156ff0e7341e6c47c4ad6672a
Anoop Varghese <anoopedicula@gmail.com>
2019-09-22 15:59:05 +0530

    error handling for email already exists

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,33 +8,46 @@ const User = require('../models/user');
 
 // POST route to create new User
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        }
-        else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-            .then(result => {
-                console.log("New user created: ", result);
-                res.status(201).json({
-                    message: 'User created successfully!'
+    // check if email already registered in the system
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            // check length ts, since response wil l be null array if not exists
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Email already registered!"
                 });
-            })
-            .catch(err => {
-                console.log("Error creating user: ", err);
-                res.status(500).json({
-                    error: err
+            }
+            else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    }
+                    else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log("New user created: ", result);
+                                res.status(201).json({
+                                    message: 'User created successfully!'
+                                });
+                            })
+                            .catch(err => {
+                                console.log("Error creating user: ", err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
                 });
-            });
-        }
-    });
+            }
+        });
 });
 
 
